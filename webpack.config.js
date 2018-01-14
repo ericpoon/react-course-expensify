b439a2dc153a781231d6d056af414cfd45654088
PAN Zhiqi <ericpoon510@gmail.com>
2018-01-14 22:00:47 +0800

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,40 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/,
-        }, {
-            test: /\.s?css$/,
-            use: ['style-loader', 'css-loader', 'sass-loader'],
-        }],
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true,
-    },
+module.exports = (env, argv) => {
+    const isProduction = env === 'production';
+    const cssExtract = new ExtractTextPlugin('styles.css');
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js',
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+            }, {
+                test: /\.s?css$/,
+                use: cssExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {sourceMap: true},
+                        }, {
+                            loader: 'sass-loader',
+                            options: {sourceMap: true},
+                        },
+                    ],
+                }),
+            }],
+        },
+        plugins: [cssExtract],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true,
+        },
+    };
 };
