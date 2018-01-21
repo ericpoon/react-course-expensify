3c06e7535585bf8ca94c3f6613f0530ba8e75f58
PAN Zhiqi <ericpoon510@gmail.com>
2018-01-21 15:17:39 +0800

    Add loader

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import AppRouter, {history} from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import {startSetExpenses} from './actions/expenses';
 import {login, logout} from './actions/auth';
+import LoadingPage from './components/LoadingPage';
 
 import 'normalize.css/normalize.css';
 
@@ -30,7 +31,7 @@ const renderApp = () => {
     }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage/>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
